bc8bd6fe93da9c22dd05294da6046cc854d8fd4f
code-house10 <107010368+code-house10@users.noreply.github.com>
2023-05-23 02:46:07 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("callButton").addEventListener("click", function() {
+  // Replace the telephone number with your desired phone number
+  window.location.href = "tel:+1234567890";
+});
